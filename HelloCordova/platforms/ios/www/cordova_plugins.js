b27f14ea9744ba1323c428ad400d3b2530297086
Aditi3 <aditi.gvc@gmail.com>
2020-12-28 17:15:13 +0530

    Add CleverTap Cordova SDK

--- HelloCordova/platforms/ios/www/cordova_plugins.js
+++ HelloCordova/platforms/ios/www/cordova_plugins.js
@@ -1,6 +1,16 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-  module.exports = [];
+  module.exports = [
+    {
+      "id": "clevertap-cordova.CleverTap",
+      "file": "plugins/clevertap-cordova/www/CleverTap.js",
+      "pluginId": "clevertap-cordova",
+      "clobbers": [
+        "CleverTap"
+      ]
+    }
+  ];
   module.exports.metadata = {
-    "cordova-plugin-whitelist": "1.3.4"
+    "cordova-plugin-whitelist": "1.3.4",
+    "clevertap-cordova": "2.3.1"
   };
 });
