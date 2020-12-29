99ae1d78c0b8332b1eaf9a81e0f7658ccb972c60
Aditi3 <aditi.gvc@gmail.com>
2020-12-29 08:54:19 +0530

    Integrate CleverTap SDK

--- HelloCordova/www/js/index.js
+++ HelloCordova/www/js/index.js
@@ -20,10 +20,22 @@
 // Wait for the deviceready event before using any of Cordova's device APIs.
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 document.addEventListener('deviceready', onDeviceReady, false);
+document.addEventListener('onCleverTapInboxDidInitialize', this.onCleverTapInboxDidInitialize, false);
 
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');
+    
+    CleverTap.setDebugLevel(3);
+    CleverTap.registerPush();
+    
+    CleverTap.recordEventWithName("foo");
+    CleverTap.recordEventWithName("User Logout");
+    CleverTap.initializeInbox();
+}
+
+function onCleverTapInboxDidInitialize() {
+    CleverTap.showInbox({"navBarTitle":"My App Inbox","tabs": ["tag1", "tag2"],"navBarColor":"#FF0000"});
 }
